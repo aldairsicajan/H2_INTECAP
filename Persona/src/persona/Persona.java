fae95db12b666da2c04e86b2484e1ddf6998aa60
aldairsicajan <aldairsicajan@gmail.com>
2023-08-12 17:12:00 -0600

    Agregando Bucles

---
+++ Persona/src/persona/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package persona;
+
+/**
+ *
+ * @author WorkHome
+ */
+public class Persona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        cPersona p1 = new cPersona(12452134, "Juan Carlos", "Villa Nueva", "45216547", "juan@email.com");
+        pi.verDatos();
+    }
+    
+}
